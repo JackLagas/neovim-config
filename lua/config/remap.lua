61c3c174561717c40779c33dc7cac54195286930
Jack Lagas <jacklagasw@gmail.com>
2023-11-08 19:03:34 -0800

    Initial Commit

---
+++ lua/config/remap.lua
@@ -0,0 +1,33 @@
+-- set map leader to space
+vim.g.mapleader = " "
+
+-- open netrw menu
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- allow moving selected text with auto-indenting
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- stop cursor from jumping around during various actions
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzb")
+
+
+-- paste without yanking
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- use ctrl+c to exit insert mode normally
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- disable Q
+vim.keymap.set("n", "Q", "<nop>")
+
+
---
+++ lua/config/set.lua
@@ -0,0 +1,40 @@
+-- add line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- set tab size to 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- automatically indent into blocks
+vim.opt.smartindent = true
+
+-- disable text wrap
+vim.opt.wrap = false
+
+-- backup to file
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- dynamically highlight text during search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- use terminal colors
+vim.opt.termguicolors = true
+
+-- keep cursor 8 lines above and below top and bottom
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- fast update time
+vim.opt.updatetime = 50
+
+-- add column 80 characters to the right
+vim.opt.colorcolumn = "80"
+
